79f045b825a728111722ff93530181fb897200dd
Andrew Tridgell <tridge@samba.org>
2009-01-14 15:35:53 +1100

    added info on timetable and fees

--- index.php
+++ index.php
@@ -10,8 +10,7 @@ include "8440head.php";
 Free and Open Source Software Development
 <br></font><font size=+2>Autumn 2009</font></h1>
 Course staff:
-<A href="http://samba.org/tridge">Dr Andrew Tridgell</A> (lecturer)<br>
-<A href="http://cs.anu.edu.au/people.php?StaffID=8909374">Bob Edwards</A><br>
+<A href="http://samba.org/tridge">Dr Andrew Tridgell</A> and <A href="http://cs.anu.edu.au/people.php?StaffID=8909374">Bob Edwards</A><br>
 </center>
 <p>
 
@@ -31,11 +30,6 @@ Software is like sex - it's better when it's free
 
 
 
-<table border="0">
-<tbody><tr>
-
-<td rowspan="3" valign="top" width="80%">
-
 <p>
 <h2>General Information</h2>
 
@@ -46,7 +40,7 @@ href="http://cs.anu.edu.au/masters">Master of Computing</a>. However,
 one-off enrollments may be made by people with the required assumed
 knowledge.  Also students taking other degrees, including 4th year
 undergraduates, may seek to enrol through special permission (contact
-the course co-ordinator). 
+the course coordinator). 
 
 <p>
 <li> Assumed knowledge is basic competence with using a Unix-like
@@ -63,34 +57,28 @@ entry for Course Description and Learning Outcomes.
 <li>
 
 A total of 18 hours of lectures will be given, plus a similar amount
-of tutorial/lab hourse. The course will have a very practical focus.
+of tutorial/lab hours. The course will have a very practical focus.
 
 <p>
-     <li> News will be posted on the course
+<li>News will be posted on the course
 	 <a href="https://cs.anu.edu.au/streams/forum.php?ForumID=232">
 	 Announcement forum</a> (staff post only). 
 <p>
-     <li>	There is also a
+<li>There is also a
 	<a href="https://cs.anu.edu.au/streams/forum.php?ForumID=235">
 	Discussion forum</a> which will be (semi-) regularly 
 	monitored by the course staff.
+<p>
+<li>A preliminary <a href="schedule.php">timetable</a> is available
 
+<p>
+<li>Please see the <a
+href="http://studyat.anu.edu.au/courses/COMP8440;fees.html">course
+fees</a> page for information on fees for domestic and international students
 
 </ul>
 
-</td>
 
-</tr>
-
-<tr>
-
-</tr>
-
-<tr>
-
-</tr>
-
-</tbody></table>
 
 <p><div align="center">
   <?php
