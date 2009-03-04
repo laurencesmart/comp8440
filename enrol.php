e1c40eae736f08ff1cb34b25933287551fb34436
Bob Edwards <bob@cs.anu.edu.au>
2009-03-04 17:12:00 +1100

    fixed up <p> tags and added button for FLU enrolments

--- enrol.php
+++ enrol.php
@@ -13,36 +13,43 @@ include "8440head.php";
 <td rowspan="3" valign="top" width="80%">
 
 <p>
-
 This course may be undertaken as either a Masters level (ANU
 accredited) course, or as a non-accredited (no assessment) course. The
 non-accredited option is lower cost, but results in no formal grade.
+</p>
 
 <h2>Enrolling at Masters level</h2>
 
+<p>
 To enrol in COMP8440 at a masters level there are four program
 options: 
-  <ul>
-    <li><a href="http://studyat.anu.edu.au/programs/7705XMCOMP;overview.html">Masters of Computing (MComp)</a></li>
-    <li><a href="http://studyat.anu.edu.au/programs/7704XMITS%20;overview.html">Masters of Information Technology (MIT)</a></li>
-    <li>Masters of Graduate Studies</li>
-    <li>Non-award enrolment</li>
-  </ul>
+</p>
+<ul>
+  <li><a href="http://studyat.anu.edu.au/programs/7705XMCOMP;overview.html">Masters of Computing (MComp)</a></li>
+  <li><a href="http://studyat.anu.edu.au/programs/7704XMITS%20;overview.html">Masters of Information Technology (MIT)</a></li>
+  <li>Masters of Graduate Studies</li>
+  <li>Non-award enrolment</li>
+</ul>
 
+<p>
 All four programs have the same course fees, and have the same
-pre-requisite requirements.<p>
+pre-requisite requirements.
+</p>
 
-Fee information is available on
-the <a href="http://studyat.anu.edu.au/courses/COMP8440;fees.html">course
-fees</a> page. Note that the fees are different for domestic and
-international students.<p>
+<p>
+Fee information is available on the
+<a href="http://studyat.anu.edu.au/courses/COMP8440;fees.html">course fees</a>
+page. Note that the fees are different for domestic and international students.
+</p>
 
+<p>
 For all programs except the Masters of Computing, students must obtain
 a permission code from the Department of Computer Science
 office. Requests for a permission code should be emailed
 to <a href="mailto:office@cs.anu.edu.au">office@cs.anu.edu.au</a>. You
 should include details of any experience or expertise that may be
 relevant for the satisfaction of the course pre-requisites. 
+</p>
 
 <h3>Pre-requisites (accredited programs only)</h3>
 
@@ -52,39 +59,61 @@ in at least one programming language will be an advantage.
 
 <h3>Enrolment for MComp/MIT students</h3>
 
+<p>
 Students undertaking the course as part of a Masters of Computing or
 Masters of Information Technology program can enrol
 using <a href="http://isis.anu.edu.au">ISIS</a>.
-
+</p>
 
 <h3>Enrolment for Masters of Graduate Studies students</h3>
 
-Students undertaking the course as part of a Masters of Graduate
-Studies
-can <a href="http://studyat.anu.edu.au/programs/7990XGRADS;overview.html">apply
-online</a>, using a program name of "GRADUATE STUDIES" and a code
-number 7990XGRADS.
-
+<p>
+Students undertaking the course as part of a Masters of Graduate Studies can
+<a href="http://studyat.anu.edu.au/programs/7990XGRADS;overview.html">apply online</a>
+, using a program name of "GRADUATE STUDIES" and a code number 7990XGRADS.
+</p>
 
 <h3>Enrolment for non-award students</h3>
 
+<p>
 Students enrolling in COMP8440 as an accredited, non-award program
-should submit
-an <a href="http://www.anu.edu.au/sas/forms/sas36b.pdf">enrolment
-  application form</a> to the address listed on the form.
+should submit an
+<a href="http://www.anu.edu.au/sas/forms/sas36b.pdf">enrolment application form</a>
+to the address listed on the form.
+</p>
+
+<br>
+<br>
 
 <h2>Enrolling as a non-accredited student</h2>
 
+<p>
 The COMP8440 course may alternatively be taken as a non-accredited
 course. This is a cheaper option (the course fee for 2009 is $1750)
 and involves no assessment. A certificate of completion will be
 awarded. The university will not provide any grading of students
 performance in the course.
+</p>
 
-<p>An online form for applying as a non-accredited student will be made
-available on this web page shortly. In the meantime, please send
-expressions of interest
-to <a href="comp8440@cs.anu.edu.au">comp8440@cs.anu.edu.au</a>.
+<form method="get" action="http://onestop.anu.edu.au/cfrstep1.cgi">
+<input name="TRAN-NO" type="hidden" value="200">
+<input name="EVENT" type="hidden" value="FOSSD" size="30">
+<input name="SEATS" type="hidden" value="1">
+<input name="VOUCHER" type="hidden" value="">
+<input name="ATTENDEE" type="hidden" value="">
+
+<p>
+An online form for applying as a non-accredited student is available here:
+<input type="submit" value="  Register Now  ">
+</p>
+
+<p>
+Please have your credit card details available. If you have any enquiries
+about registering as a non-accredited student, please contact us at:
+<a href="comp8440@cs.anu.edu.au">comp8440@cs.anu.edu.au</a>.
+</p>
+
+</form>
 
 </td>
 </tr>
