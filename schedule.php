0f77ba3fffd4656dbec7ec07366e525a6b433f61
Andrew Tridgell <tridge@samba.org>
2009-04-09 18:38:30 +1000

    added more detailed schedule

--- schedule.php
+++ schedule.php
@@ -42,6 +42,73 @@ students are expected to prepare materials for assessment, which will
 be due by Friday the 22nd of May. Details on assessment options will
 be made available on this website closer to the start of the course.
 
+<h2>Detailed Schedule</h2>
+
+The following is a more detailed schedule for the 5 day intensive part
+of the course. 
+
+<pre>
+Tuesday
+ 9:00 introduction and welcome
+ 9:30 Lecture: An introduction to FOSS
+10:30 morning tea
+10:45 Lab: Install a FOSS project - all on one project
+12:00 Lecture: Getting started in a FOSS project
+13:00 Lunch
+14:00 Lecture: How are FOSS projects governed?
+15:00 Afternoon tea
+15:15 Lab: Installing a FOSS project - choice of 5
+17:45 close
+
+Wednesday
+ 9:00 introducing the course projects
+ 9:30 Lecture: The history of FOSS
+10:30 morning tea
+10:45 Lab: finding a FOSS project
+12:00 Lecture: Inside FOSS licensing
+13:00 Lunch
+14:00 Lecture: FOSS and the law
+15:00 Afternoon tea
+15:15 Lab: study of chosen FOSS project
+17:45 close
+
+Thursday
+ 9:00 discussion of FOSS project work
+ 9:30 Lecture: Source code management for FOSS projects
+10:30 morning tea
+10:45 Lab: getting in touch with the project
+12:00 Lecture: FOSS and business
+13:00 Lunch
+14:00 Lecture: What motivates a FOSS developer?
+15:00 Afternoon tea
+15:15 Lab: project work
+17:45 close
+
+Friday
+ 9:00 discussion of FOSS project work
+ 9:30 Lecture: Case study: Samba
+10:30 morning tea
+10:45 Lab: project work
+12:00 Lecture: FOSS distributions and platforms
+13:00 Lunch
+14:00 Lecture: FOSS and Security
+15:00 Afternoon tea
+15:15 Lab: project work
+17:45 close
+
+Saturday
+ 9:00 discussion of FOSS project work
+ 9:30 Lecture: Starting a new project
+10:30 morning tea
+10:45 Lab: project presentations
+12:00 Lecture: FOSS Tales
+13:00 Lunch
+14:00 Lecture: Release early, release often
+15:00 Afternoon tea
+15:15 Lab: project presentations
+17:45 close
+</pre>
+
 </td>
 </tr>
 
